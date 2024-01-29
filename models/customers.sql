0d65c2920ffe712d26dd6ec361a8688604482eb3
Abraham Lincoln <njoguabraham2@gmail.com>
2024-01-29 14:11:53 +0000

    Creating dependencies for models

--- models/customers.sql
+++ models/customers.sql
@@ -1,19 +1,17 @@
 with customers as (
-    select
-        id as customer_id,
-        first_name,
-        last_name
-    from raw.jaffle_shop.customers
+
+    select * from {{ ref('stg_customers') }}
+
 ),
+
 orders as (
-    select 
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-    from raw.jaffle_shop.orders
+
+    select * from {{ ref('stg_orders') }}
+
 ),
+
 customer_orders as (
+
     select
         customer_id,
 
@@ -24,7 +22,9 @@ customer_orders as (
     from orders
 
     group by 1
+
 ),
+
 final as (
 
     select
